e19ae80291a508e94a21ccbd52833c7629605070
Shayla Shahar <97187728+shaylashahar@users.noreply.github.com>
2022-09-14 18:20:18 -0700

    Update Kaiju_GTDB.py

--- Kaiju_GTDB.py
+++ Kaiju_GTDB.py
@@ -9,14 +9,15 @@ directory = "./bacteria"
 fout=open("proteins.faa", "w")
 for file in os.listdir(directory): #loop through files
     with open("./bacteria/" + file) as f:
+        #reads each file and finds accession number
         contents = f.readlines()
         first = contents[0].strip()
         per=first.find(".")
         acc= first[1:per]
+        #uses accession number to search NCBI for taxid
         taxid = subprocess.check_output(f'efetch -db nuccore -id {acc} -format docsum | xtract -pattern DocumentSummary -element TaxId', shell=True)
         taxid = taxid.decode('utf-8')
-        print(taxid)
-        for line in contents:
+        for line in contents: #writes new file with tax id followed by sequence 
             if line.startswith(">"):
                 fout.write(">" + str(taxid))
             else:
