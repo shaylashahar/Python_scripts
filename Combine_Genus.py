57ca146b5bb96e7de4ff862c87f4546bf5f3dac2
Shayla Shahar <97187728+shaylashahar@users.noreply.github.com>
2022-12-06 13:38:47 -0800

    Update Combine_Genus.py

--- Combine_Genus.py
+++ Combine_Genus.py
@@ -3,8 +3,8 @@
 from operator import mod
 import pandas as pd 
 
-original_df = pd.read_csv("/Users/shayla/Library/Mobile Documents/com~apple~CloudDocs/School/Graduate/Kelley Lab/TJ_Y2/SourceTracker/MetagenomeOTUs/soil_OTU_bacteria.csv")
+original_df = pd.read_csv("/Users/shayla/MetagenomeOTUs/soil_OTU_bacteria.csv")
 mod_df = original_df.dropna(how='any', subset=['genus'])
 final_df = mod_df.groupby('genus').sum()
-final_df.to_csv("/Users/shayla/Library/Mobile Documents/com~apple~CloudDocs/School/Graduate/Kelley Lab/TJ_Y2/SourceTracker/MetagenomeOTUs/GENUS_soil_OTU_bacteria.csv")
+final_df.to_csv("/Users/shayla/MetagenomeOTUs/GENUS_soil_OTU_bacteria.csv")
 
